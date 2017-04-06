7a00c4100343e1b999ee9eb579d201e14bed852e
jioong <ljiong8907@163.com>
2017-04-06 23:32:46 +0800

    泛型

---
+++ advanced-features/src/main/java/com/github/jioong/advanced/reflection/App.java
@@ -0,0 +1,16 @@
+package com.github.jioong.advanced.reflection;
+
+/**
+ * Created by jioong on 17-4-6.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Class clazz = App.class;
+
+        System.out.println(clazz.getPackage());
+    }
+
+
+
+}
